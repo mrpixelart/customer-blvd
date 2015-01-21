0a15a27489d9039998cbf140094314f44946d58d
Mitch Ray <mitchray90@gmail.com>
2015-01-21 16:52:18 -0600

    initial wordpress setup

--- wp-config.php
+++ wp-config.php
@@ -86,19 +86,19 @@ define('WPLANG', '');
  * Set custom paths
  *
  * These are required because wordpress is installed in a subdirectory.
- */
-if (!defined('WP_SITEURL')) {
-	define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
-}
-if (!defined('WP_HOME')) {
-	define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '');
-}
-if (!defined('WP_CONTENT_DIR')) {
-	define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
-}
-if (!defined('WP_CONTENT_URL')) {
-	define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
-}
+//  */
+// if (!defined('WP_SITEURL')) {
+// 	define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+// }
+// if (!defined('WP_HOME')) {
+// 	define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '');
+// }
+// if (!defined('WP_CONTENT_DIR')) {
+// 	define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
+// }
+// if (!defined('WP_CONTENT_URL')) {
+// 	define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+// }
 
 
 /**
