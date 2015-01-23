d1f02f5ccc6fff95567bb8dc44033dbbd11ac520
Mitch Ray <mitchray90@gmail.com>
2015-01-22 19:43:30 -0600

    act working, added first repeater section

--- wp-content/themes/my-theme/home-page.php
+++ wp-content/themes/my-theme/home-page.php
@@ -17,11 +17,40 @@ get_header();
 
 			<img src="<?php the_field('hero_image'); ?>" />
 
-			<p><?php the_content(); ?></p>
+			<h2><?php the_field('process_headline'); ?></h2>
 
-		<?php endwhile; // end of the loop. ?>
 
-	</div><!-- #content -->
+			<?php if( have_rows('process_info') ): ?>
+
+				<ul class="process-steps">
+
+					<?php while( have_rows('process_info') ): the_row(); 
+
+					// vars
+					$image = get_sub_field('process_image');
+					$name = get_sub_field('process_name');
+					$description = get_sub_field('process_description');
+
+					?>
+
+					<li class="step">
+						<h3><?php echo $name; ?></h3>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+						<p>	<?php echo $description; ?></p>
+					</li>
+
+				<?php endwhile; ?>
+
+			</ul>
+
+		<?php endif; ?>
+
+
+		<p><?php the_content(); ?></p>
+
+	<?php endwhile; // end of the loop. ?>
+
+</div><!-- #content -->
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
