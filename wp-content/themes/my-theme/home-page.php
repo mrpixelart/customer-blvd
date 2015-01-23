7e7522b778fe59cf4f267064ee5b39a553369926
Mitch Ray <mitchray90@gmail.com>
2015-01-22 21:59:09 -0600

    added core theme files (header/footer) and linked resources in header

--- wp-content/themes/my-theme/home-page.php
+++ wp-content/themes/my-theme/home-page.php
@@ -2,6 +2,9 @@
 
 /**
  * Template Name: Home Page
+ *
+ * @package Customer-Blvd
+ *
  */
 
 get_header(); 
@@ -9,46 +12,68 @@ get_header();
 ?>
 
 <div id="primary">
+
 	<div id="content" role="main">
+		
+		<div class="hero">
+			<div class="container ">
+				<h1 class="notop"><?php the_field('hero_headline'); ?></h1>
+				<p class="lead "><?php the_field('hero_description'); ?></p>
+				<a class="waves-effect waves-light btn-large"><?php the_field('hero_button'); ?></a>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<h2><?php the_field('process_headline'); ?></h2>
 
-		<?php while ( have_posts() ) : the_post(); ?>
 
-			<h1><?php the_field('custom_title'); ?></h1>
+				<?php if( have_rows('process_info') ): ?>
 
-			<img src="<?php the_field('hero_image'); ?>" />
+					<ul class="process-steps">
 
-			<h2><?php the_field('process_headline'); ?></h2>
-
-
-			<?php if( have_rows('process_info') ): ?>
-
-				<ul class="process-steps">
-
-					<?php while( have_rows('process_info') ): the_row(); 
+						<?php while( have_rows('process_info') ): the_row(); 
 
 					// vars
-					$image = get_sub_field('process_image');
-					$name = get_sub_field('process_name');
-					$description = get_sub_field('process_description');
+						$image = get_sub_field('process_image');
+						$name = get_sub_field('process_name');
+						$description = get_sub_field('process_description');
 
-					?>
+						?>
 
-					<li class="step">
-						<h3><?php echo $name; ?></h3>
-						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-						<p>	<?php echo $description; ?></p>
-					</li>
+						<div class="col s12 m4">
+							<div class="card">
+								<div class="card-image icon-image waves-effect waves-block waves-light">
+									<div class="valign-wrapper">
+										<i class="activator valign fa fa-4x <?php echo $image; ?>"></i>
+									</div>
+								</div>
+								<div class="card-content">
+									<span class="card-title activator grey-text text-darken-4"><?php echo $name; ?> <i class="mdi-navigation-more-vert right"></i></span>
 
-				<?php endwhile; ?>
+								</div> 
+								<div class="card-reveal">
+									<span class="card-title grey-text text-darken-4"><?php echo $name; ?> <i class="mdi-navigation-close right"></i></span>
+									<p><?php echo $description; ?></p>
+								</div>
+							</div>
 
-			</ul>
-
-		<?php endif; ?>
+						</div>
 
 
-		<p><?php the_content(); ?></p>
 
-	<?php endwhile; // end of the loop. ?>
+					<?php endwhile; ?>
+
+				</ul>
+
+			<?php endif; ?>
+
+
+			<p><?php the_content(); ?></p>
+
+
+
+		</div>
+	</div>
 
 </div><!-- #content -->
 </div><!-- #primary -->
